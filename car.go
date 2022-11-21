5d725d9d820d7a2f52467155d21184feccfbcf23
saintvrai <ishanovairat@gmail.com>
2022-11-21 22:36:11 +0300

    Z1| GETALL function all done

--- car.go
+++ car.go
@@ -2,6 +2,6 @@ package Drom
 
 type Car struct {
 	ID       string `json:"id" db:"id"`
-	Name     string `json:"name" db:"id"`
-	CarBrand string `json:"carBrand" db:"id"`
+	Name     string `json:"name" db:"name"`
+	CarBrand string `json:"carBrand" db:"carbrand"`
 }
